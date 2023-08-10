758df1e1a8ffb5ce1c9e48de92343e43faf57f1e
musabah00 <eng.musab.bme@gmail.com>
2023-08-10 06:02:25 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.c>
+/**
+ * main - A program that print the size of data type
+ * Return: 0
+ */
+int main(void)
+{
+printf("Size of a char: %c\n", sizeof(char));
+printf("Size of a int: %ld\n", sizeof(int));
+printf("Size of a long int: %lld\n", sizeof(char));
+printf("Size of a long long int: %c\n", sizeof(char));
+printf("Size of a flaot: %f\n", sizeof(float));
+return (0);
+}
