c67a6345a3e1a5d2a0e9db9cd0e668732410706e
musabah00 <eng.musab.bme@gmail.com>
2023-08-10 06:06:05 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 printf("Size of a char: %c\n", sizeof(char));
 printf("Size of a int: %ld\n", sizeof(int));
-printf("Size of a long int: %lld\n", sizeof(char));
-printf("Size of a long long int: %c\n", sizeof(char));
+printf("Size of a long int: %lld\n", sizeof(long int));
+printf("Size of a long long int: %c\n", sizeof(long long int));
 printf("Size of a flaot: %f\n", sizeof(float));
 return (0);
 }
