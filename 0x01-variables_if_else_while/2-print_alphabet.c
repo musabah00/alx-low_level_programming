d7e0320a21dd61bae972972f6c74bc097c1674f0
musabah00 <eng.musab.bme@gmail.com>
2023-08-11 23:16:51 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program print alphaebt from A to Z
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
