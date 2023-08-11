5872f47b9f82d3fc66e8d329b464096efd33ac1e
musabah00 <eng.musab.bme@gmail.com>
2023-08-12 00:03:27 +0200

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that print number from 0 to 9 using function putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
